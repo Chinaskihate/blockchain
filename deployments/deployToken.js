5625b572c6030da54a57ea1e6922c7ae9ea3f588
Chinaskihate <islam.kadyrov.dlschool@gmail.com>
2022-10-15 23:56:38 +0300

    Fixed deploying script

--- deployments/deployToken.js
+++ deployments/deployToken.js
@@ -1,18 +1,18 @@
-const { ethers } = require("hardhat")
+const { ethers } = require("hardhat");
 
 const main = async () => {
+  const initialSupply = ethers.utils.parseEther("100000");
+  const [deployer] = await ethers.getSigners();
+  console.log(`Address deploying the contract --> ${deployer.address}`);
+  const tokenFactory = await ethers.getContractFactory("MyToken");
+  const contract = await tokenFactory.deploy(initialSupply, "MyToken");
 
-    const initialSupply = ethers.utils.parseEther("100000");
-    const [deployer] = await ethers.getSigners();
-    console.log(`Address deploying the contract --> ${deployer.address}`);
-    const tokenFactory = await ethers.getContractFactory("Token");
-    const contract = await tokenFactory.deploy(initialSupply);
-
-    console.log(`Toen Contract address --> ${contract.address}`);
-}
+  console.log(`Token Contract address --> ${contract.address}`);
+};
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);``
-    })
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+    ``;
+  });
